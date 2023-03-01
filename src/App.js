a265f00ce4e14ea40dec4d70e6006cc39b71681e
Thomas Mouchelet <mouchelet.thomas@gmail.com>
2023-03-01 10:07:21 +0100

    pre-prod

--- src/App.js
+++ src/App.js
@@ -2,6 +2,23 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const handleSignup = async () => {
+    const randomEmail = Math.random().toString(36).substring(7);
+    console.log(randomEmail);
+    const response = await fetch(`${process.env.REACT_APP_API_URL}/auth/signup`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        email: `${randomEmail}@gmail.com`,
+        password: "password"
+      })
+    })
+    const data = await response.json()
+    console.log("response signup", data);
+  }
+
   return (
     <div className="App">
       <header className="App-header">
@@ -9,6 +26,11 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <button
+          onClick={handleSignup}
+        >
+          Signup
+        </button>
         <a
           className="App-link"
           href="https://reactjs.org"
